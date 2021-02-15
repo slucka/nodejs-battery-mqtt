628d7ab06026d9d29107f2a1696eae60fb103e10
slucka <slucka@gmail.com>
2021-02-15 08:50:52 -0300

    20211215

--- batterymqtt.js
+++ batterymqtt.js
@@ -1,15 +1,17 @@
 const mqtt = require('mqtt');
 const batteryLevel = require('battery-level');
+const ping = require('ping');
 
 const deviceId = 'oblap002';
 const mqttServer = 'mqtt://192.168.1.107';
 const mqttClientId = 'mqttjs01';
 
+//var hosts = ['192.168.182.129', 'google.com', 'yahoo.com'];
+//var vm_ip = '192.168.182.129';
+var vm_ip = 'VM-WIN10-DESA';
+
 var client = null;
-
-
 client = mqtt.connect(mqttServer, { clientId: mqttClientId });
-
 client.on("connect", function () {
     console.log("connected");
     client.publish("hello", "hello world");
@@ -31,36 +33,79 @@ function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
 
+function sendMttqData(level){
+
+    var valueRef = parseInt(level * 100);
+
+    var data1 = {
+        "id" : deviceId,
+        "param": "thermostatTemperatureAmbient",
+        "value": valueRef,
+        "intent": "rules",
+    };
+    var json = JSON.stringify(data1);
+    console.log("publish: " + json);
+    client.publish("device/control", json);
+
+    var data2 = {
+        "id" : deviceId,
+        "param": "temperature",
+        "value": valueRef,
+        "intent": "rules",
+    };
+    json = JSON.stringify(data2);
+    console.log("publish: " + json);
+    client.publish("device/control", json);
+}
+
 setInterval(function () {
 
+    // batteryLevel().then(level => {
+
+    //     var valueRef = parseInt(level * 100);
+
+    //     var data1 = {
+    //         "id" : deviceId,
+    //         "param": "thermostatTemperatureAmbient",
+    //         "value": valueRef,
+    //         "intent": "rules",
+    //     };
+    //     var json = JSON.stringify(data1);
+    //     console.log("publish: " + json);
+    //     client.publish("device/control", json);
     
-
-    batteryLevel().then(level => {
-
-        var valueRef = parseInt(level * 100);
-
-        var data1 = {
-            "id" : deviceId,
-            "param": "thermostatTemperatureAmbient",
-            "value": valueRef,
-            "intent": "rules",
-        };
-        var json = JSON.stringify(data1);
-        console.log("publish: " + json);
-        client.publish("device/control", json);
-    
-        var data2 = {
-            "id" : deviceId,
-            "param": "temperature",
-            "value": valueRef,
-            "intent": "rules",
-        };
-        json = JSON.stringify(data2);
-        console.log("publish: " + json);
-        client.publish("device/control", json);
+    //     var data2 = {
+    //         "id" : deviceId,
+    //         "param": "temperature",
+    //         "value": valueRef,
+    //         "intent": "rules",
+    //     };
+    //     json = JSON.stringify(data2);
+    //     console.log("publish: " + json);
+    //     client.publish("device/control", json);
 
     
+    // });
+
+
+
+    // hosts.forEach(function(host){
+    //     ping.sys.probe(host, function(isAlive){
+    //         var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
+    //         console.log(msg);
+    //     });
+    // });
+
+    ping.sys.probe(vm_ip, function(isAlive){
+        if (isAlive){
+            console.log('############### Is Not Alive ###############');
+            sendMttqData(0.1)
+        }else{
+            console.log('############### Is Alive ###############');
+            batteryLevel().then(level => {
+                sendMttqData(level)
+            });
+        }
     });
-
 
 }, 10000);
